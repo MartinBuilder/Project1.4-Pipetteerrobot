790dbf5cf68b6c797eb38334d42ebbbcbe747e6d
KoenSli <koenslijkerman8@gmail.com>
2023-06-05 16:25:29 +0200

    Feat/ZwarteKnop

---
+++ Interface.c
@@ -0,0 +1,30 @@
+#inculde "Interface.h"
+
+int Sum(int a, int b)
+{
+   return a+b;
+}
+
+int Zwarteknop()
+{
+
+input_b(2);
+   return 1;
+   
+
+}
+
+int Kliksensor()
+{
+
+}
+
+int LSreservoir()
+{
+
+}
+
+int LSbuisjes()
+{
+
+}
